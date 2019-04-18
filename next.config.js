aabb76a4f7bc34c52b8df8fd11b55fa1917074ed
Conner Wells <cwells@connectivedx.com>
2019-04-18 11:56:33 -0700

    sets up custom Webpack config for aliasing, static asset copying, plugin provision.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,44 @@
+const webpack = require('webpack')
+const path = require('path')
+const pkgpath = require('packpath')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 const withCSS = require('@zeit/next-css')
 const withMDX = require('@zeit/next-mdx')
 
-const config = {
+// grab what we need from package.json
+const { directories } = require(path.resolve(pkgpath.self(), 'package.json'))
+const source = (...paths) => path.resolve(pkgpath.self(), directories.source, ...paths)
+
+module.exports = withMDX({ })(withCSS({
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]'
   },
   pageExtensions: ['js', 'jsx', 'mdx'],
-  webpack: config => {
+  webpack: (config, { dev }) => {
     config.resolve.extensions.push('.mdx')
+    config.resolve.alias = {
+      '@atoms': source('atoms'),
+      '@molecules': source('molecules'),
+      '@organisms': source('organisms'),
+      '@templates': source('templates'),
+      ...config.resolve.alias
+    }
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react',
+        PropTypes: 'prop-types'
+      }),
+      new CopyWebpackPlugin([{
+        from: source('static'),
+        to: 'assets/static'
+      }], {
+        ignore: ['README.md']
+      })
+    )
+
     return config
   }
-}
-
-module.exports =
-  withMDX({ })(
-    withCSS(config)
-  )
+}))
--- src/atoms/PageShell/PageShell.jsx
+++ src/atoms/PageShell/PageShell.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
 export const PageShell = (props) => {
   const {
     className,
