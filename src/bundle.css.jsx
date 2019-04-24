2dcf082b2c20f3c15a0d90c71fb9b69b8232a882
Conner Wells <cwells@connectivedx.com>
2019-04-24 13:49:55 -0700

    moves ordering around for better.... better everything :)

--- src/bundle.css.jsx
+++ src/bundle.css.jsx
@@ -1,8 +1,8 @@
-const requireAll = (context) => context.keys().map(context);
 require('./static/reset.css');
 require('./static/variables/index.css');
 require('@atoms/MinotaurGlobal/MinotaurGlobal.css');
 
+const requireAll = (context) => context.keys().map(context);
 
 // require the rest of the components
 requireAll(require.context('@atoms/', true, /\.css$/));
