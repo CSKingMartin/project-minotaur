156c41003a769655872970953426e9316655804e
Conner Wells <conner.c.wells@gmail.com>
2019-05-31 18:24:42 -0700

    adds Brand component

--- src/molecules/Brand/index.jsx
+++ src/molecules/Brand/index.jsx
@@ -1,22 +1,50 @@
+import Heading from '@atoms/Heading';
+import Image from '@atoms/Image';
+import { Media, Media__body, Media__figure } from '@molecules/Media';
+import Link from 'next/link';
+
 export const Brand = (props) => {
 	const {
 		className,
+		variant,
 		children,
 		...rest
 	}	= props;
 
-	const stack = utilities.createClassStack = ([
+	const stack = utilities.createClassStack([
 		'Brand',
+		`Brand--${variant}`,
 		className
 	]);
 
 	return (
-		<div className={stack}>Hi mom</div>
+		<div className={stack} {...rest}>
+			<Link
+				href="/"
+				className={stack}
+			>
+				<a className="Brand__wrapper">
+					<Media align="center">
+						<Media__body>
+							<Heading className="Brand__script" level="h3">Minotaur</Heading>
+						</Media__body>
+						<Media__figure>
+							<Image className="Brand__logo" src="/static/images/minotaur-color.png" />
+						</Media__figure>
+					</Media>
+				</a>
+			</Link>
+		</div>
 	);
+};
+
+Brand.defaultProps = {
+	variant: 'default'
 };
 
 Brand.propTypes = {
 	className: PropTypes.string,
+	variant: PropTypes.oneOf(['default', 'dark']),
 	children: PropTypes.node
 };
 
