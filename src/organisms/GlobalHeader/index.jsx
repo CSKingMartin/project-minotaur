6912a2bc2d78457629bae386d53feedc830b065f
Conner Wells <conner.c.wells@gmail.com>
2019-06-10 06:42:42 -0700

    finishes 'Globalheader__dropdown' refactor, full refactor still WIP

--- src/organisms/GlobalHeader/index.jsx
+++ src/organisms/GlobalHeader/index.jsx
@@ -6,6 +6,8 @@ import Heading from '@atoms/Heading/';
 import Nup from '@atoms/Nup';
 import NavList from '@molecules/NavList';
 import Wrapper from '@atoms/Wrapper';
+import Dropdown from '@organisms/GlobalHeader/Dropdown.jsx';
+import PanelMenu from '@organisms/GlobalHeader/PanelMenu.jsx';
 
 export class GlobalHeader extends React.Component {
 	constructor(props) {
@@ -48,54 +50,31 @@ export class GlobalHeader extends React.Component {
 		]);
 
 		return (
-			<div data-hover={true} className={stack} {...rest}>
+			<div data-hover="Root" className={stack} {...rest}>
 				<Wrapper className="GlobalHeader__wrapper" size="default">
-					<div className="GlobalHeader__primary" data-hover={true}>
-						<div className="GlobalHeader__logo">
-							<Brand variant="dark" />
-						</div>
-						<div
-							className={`GlobalHeader__nav-item ${this.state.isActive ? 'is-active' : ''}`}
-							onMouseEnter={this.onHover}
-							onMouseLeave={(e) => this.onLeave(e)}
-						>
-							<div className="GlobalHeader__nav-heading">Catalog</div>
-						</div>
-						<div className="GlobalHeader__nav-item">
-							<a href="/docs">
-								<div className="GlobalHeader__nav-heading">Docs</div>
-							</a>
-						</div>
-						<div className="GlobalHeader__github">
-							<a href="https://www.github.com/CSKingMartin/project-minotaur" alt="Link to Github repository homepage">
-								<Image src="/static/images/github.svg" />
-							</a>
-						</div>
+					<div className="GlobalHeader__bin" data-hover="Root">
+					  <div className="GlobalHeader__logo" data-hover="Close">
+					    <Brand variant="dark" />
+					  </div>
+					  <Dropdown href="/catalog" heading="Catalog">
+					    <PanelMenu data-hover="catalogNavHover">
+					      <PanelMenu.Sidebar>
+					        <Heading level="h6">Pages</Heading>
+					        <p>No pages :(</p>
+					      </PanelMenu.Sidebar>
+					      <PanelMenu.Main>
+					        <NavList label="atoms" />
+					        <NavList label="molecules" />
+					        <NavList label="organisms" />
+					      </PanelMenu.Main>
+					    </PanelMenu>
+					  </Dropdown>
+					  <Dropdown href="/docs" heading="Docs">
+					    <Card data-hover="docsNavHover">
+					      Testing
+					    </Card>
+					  </Dropdown>
 					</div>
-					{/* begin full NavList panel */}
-					<Expandable
-						className="GlobalHeader__catalog-panel"
-					  toggleElement="none"
-					  closeHeight="0"
-					  forceExpand={this.state.isActive}
-					>
-					  <Card
-					  	className="GlobalHeader__catalog-inner"
-					  	onMouseLeave={this.onLeave}
-					  	data-hover={true}
-					  >
-					  	<div className="GlobalHeader__catalog-side">
-					  		<Heading level="h6">Minotaur</Heading>
-					  		<p>Eventually other links can live here.</p>
-					  		<p>Links such as Typography, colors, and fonts</p>
-					  	</div>
-					  	<Nup className="GlobalHeader__catalog-main" variant="three">
-						  	<NavList label="atoms" />
-								<NavList label="molecules" />
-								<NavList label="organisms" />
-							</Nup>
-					  </Card>
-					</Expandable>
 				</Wrapper>
 			</div>
 		);
