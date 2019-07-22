050a5bb939c07b1bd5edb8d761884d811e812dcd
Brooke <perrysbrooke@gmail.com>
2019-07-22 11:47:08 -0700

    update switchcase returns; get options props for Select

--- src/organisms/SpecimenTwo/PropsTable.jsx
+++ src/organisms/SpecimenTwo/PropsTable.jsx
@@ -1,4 +1,9 @@
 import registry from '@catalog/registry.json';
+import TextArea from '@atoms/TextArea';
+import Toggle from '@molecules/Toggle';
+import Select from '@molecules/Select';
+import Input from '@molecules/Input';
+ 
 
 export const PropsTable = (props) => {
   const {
@@ -13,14 +18,38 @@ export const PropsTable = (props) => {
     className
   ]);
 
-  const getProps = registry[query].props //gets object containing all element's props
+  const getProps = registry[query].props
+  // const getDefaultProps = registry[query]. => defaultProps not listed in registry?
+
+  const separateByPropType = (type) => {
+    if (typeof(type) !== 'object') {
+      switch(type){
+        case 'string':
+          return <Input />
+        case 'node' :
+          return <Input />;
+        case 'node.isRequired' :
+          return <Input />;
+        case 'oneOf':
+          return <Select />;
+        case 'bool':
+          return <Toggle />;
+        case 'object' :
+          return "object";
+        default : 
+          return "default return";
+      } 
+    } else 
+    console.log(getOptionsForSelect(type))
+    return <Select options={getOptionsForSelect(type)}/>
+  }
 
   // checking for PropTypes.object
   const checkPropType = (type) => {
     if (typeof(type) === 'object') {
       let typeKey = Object.keys(type);
-      let typeValues = Object.values(type).join(' ');
-      return typeKey + ': ' + typeValues
+      // let typeValues = Object.values(type).join(' ');
+      return typeKey 
     }
     else return type;
   };
@@ -31,10 +60,21 @@ export const PropsTable = (props) => {
         <div key={index} className="wrapper">
           <span  className="key-column">{getProps[prop].name}: </span>
           <span className="value-column" >{checkPropType(getProps[prop].type)}</span>
+          <span className="input-column" >{separateByPropType(getProps[prop].type)}</span>
         </div>
       );
     });
   };
+
+  const getOptionsForSelect = (type) => {
+    let keys = Object.keys(type).toString();
+    let values = Object.values(type)[0];
+    let newArray = [];
+    values.forEach(function(value){
+      newArray.push({value: value, label: value})
+    })
+    return newArray;
+  }
 
 // //practicing recursion
 //   const reverseString = (string) => {
