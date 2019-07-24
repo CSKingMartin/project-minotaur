b5adf0a82f30ea8c8516f4ac9b08ae5234b20d5c
Brooke <perrysbrooke@gmail.com>
2019-07-24 09:23:40 -0700

    change to: toggleElement: PropType.node

--- src/molecules/Expandable/index.jsx
+++ src/molecules/Expandable/index.jsx
@@ -90,10 +90,7 @@ Expandable.propTypes = {
   closeHeight: PropTypes.string,
   forceExpand: PropTypes.bool,
   forwardedRef: PropTypes.object,
-  toggleElement: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.oneOf(['none'])
-  ]),
+  toggleElement: PropTypes.node,
   children: PropTypes.node
 };
 
